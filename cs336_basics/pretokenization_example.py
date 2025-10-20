cf55f5539543560c55b772fce937e92a83eb6919
wanglezz <2655541965@qq.com>
2025-10-20 19:58:16 +0800

    sync_to_win

--- cs336_basics/pretokenization_example.py
+++ cs336_basics/pretokenization_example.py
@@ -1,5 +1,11 @@
 import os
 from typing import BinaryIO
+import regex
+from multiprocessing import Pool
+from collections import Counter,defaultdict
+
+PAT = ""
+compile_pat = regex.compile(PAT)
 
 
 def find_chunk_boundaries(
@@ -48,15 +54,36 @@ def find_chunk_boundaries(
     # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
     return sorted(set(chunk_boundaries))
 
+def process_chunk(
+    task:tuple
+) -> dict:
+    input_path,start,end = task
+    local_word_freqs = defaultdict(int)
 
-## Usage
-with open(..., "rb") as f:
-    num_processes = 4
-    boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
-    # The following is a serial implementation, but you can parallelize this
-    # by sending each start/end pair to a set of processes.
-    for start, end in zip(boundaries[:-1], boundaries[1:]):
-        f.seek(start)
-        chunk = f.read(end - start).decode("utf-8", errors="ignore")
-        # Run pre-tokenization on your chunk and store the counts for each pre-token
+def train_bpe(input_path:str,
+    vocab_size:int,
+    special_token:list[str],
+)->(dict,list):
+    # initialize vocab with special_tokens and basic 256 byte
+    vocab = {i:bytes([i])for i in range(256)}
+    current_id = 256
+    for token in special_token:
+        token_bytes = token.encode("utf-8")
+        vocab[current_id] = token_bytes
+        current_id += 1
+
+    merges = []
+    # --- pre-tokenization stage ---
+
+    with open(input_path, "rb") as f:
+        num_processes = 4
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+        # The following is a serial implementation, but you can parallelize this
+        # by sending each start/end pair to a set of processes.
+
+
+
+
+
+
