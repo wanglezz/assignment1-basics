8a6e489a36e2ae15be86ec9092c54efa37832d09
wanglezz <2655541965@qq.com>
2025-10-21 00:20:00 +0800

    sync_to_mac

--- cs336_basics/pretokenization_example.py
+++ cs336_basics/pretokenization_example.py
@@ -1,11 +1,15 @@
 import os
+import re
 from typing import BinaryIO
 import regex
 from multiprocessing import Pool
 from collections import Counter,defaultdict
 
-PAT = ""
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""" #GPT2 Regex
 compile_pat = regex.compile(PAT)
+special_tokens = ["<|endoftext|>"]
+special_pattern_str = "|".join(re.escape(s) for s in special_tokens)
+split_pattern = re.compile(f'({special_pattern_str})')
 
 
 def find_chunk_boundaries(
@@ -54,11 +58,38 @@ def find_chunk_boundaries(
     # Make sure all boundaries are unique, but might be fewer than desired_num_chunks
     return sorted(set(chunk_boundaries))
 
+# pre-tokenize chunk
 def process_chunk(
     task:tuple
 ) -> dict:
     input_path,start,end = task
+    try:
+        with open(input_path, "rb") as f:
+            f.seek(start)
+            chunk_bytes = f.read(end - start)
+            chunk_str = chunk_bytes.decode("utf-8",errors="ignore")
+    except Exception as e:
+        print(f"Reading Chunk {start}-{end}: {e}")
+        return defaultdict(int)
+
     local_word_freqs = defaultdict(int)
+
+    if split_pattern and special_pattern_str:
+        text_parts = split_pattern.split(chunk_str)
+    else:
+        text_parts = [chunk_str]
+
+    for part in text_parts:
+        if not part:
+            continue
+
+        if part in special_tokens:
+            local_word_freqs[part] += 1
+        else:
+            for match in compile_pat.finditer(part):
+                local_word_freqs[match.group(0)] += 1
+
+    return local_word_freqs
 
 
 def train_bpe(input_path:str,
@@ -72,15 +103,42 @@ def train_bpe(input_path:str,
         token_bytes = token.encode("utf-8")
         vocab[current_id] = token_bytes
         current_id += 1
-
     merges = []
     # --- pre-tokenization stage ---
 
+    # divide raw_text into chunk
     with open(input_path, "rb") as f:
         num_processes = 4
         boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
-        # The following is a serial implementation, but you can parallelize this
-        # by sending each start/end pair to a set of processes.
+
+    tasks = []
+    for start, end in zip(boundaries[:-1], boundaries[1:]):
+        if start < end:
+
+            tasks.append((input_path, start, end))
+
+    global_word_freqs = Counter()
+
+    with Pool(processes=num_processes) as pool:
+        for local_word_freqs in pool.imap_unordered(process_chunk, tasks):
+            global_word_freqs.update(local_word_freqs)
+
+    stats = {} #  dict[tuple[bytes],int]
+    for word_str, freq in global_word_freqs.items():
+        token_id_sequence = tuple(word_str.encode("utf-8"))
+        stats[token_id_sequence] = freq
+
+    # --- bpe merge stage ---
+
+
+if __name__ == '__main__':
+
+    input_path = os.path.join("..","data", "TinyStoriesV2-GPT4-valid.txt")
+
+    train_bpe(input_path,
+    4096,
+    special_tokens)
+
 
 
 
